f869a97516d6a8f287c5835e45aad699a4514bf4
Rotimi Ishola <74196357+Timiphil@users.noreply.github.com>
2022-12-08 14:24:58 +0100

    Dark mode fun

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function App() {
+    const [ darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    // console.log(toggleDarkMode)
+    
+    return (
+        <div className="container">
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode} />
+        </div>
+    )
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# co2d3485c88009d045df55f8f
+
+Quick start:
+
+```
+$ yarn # npm install
+$ yarn build # npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ yarn watch # npm run watch
+```
+
+## About the mini challenge
+
+ Fun facts about React is a little challenge to exercise my React skills for bigger projects. This shows the good part of react and how build apps with React. toggle the dark and light mode for smaller screen.
+
+Happy Coding!
+
